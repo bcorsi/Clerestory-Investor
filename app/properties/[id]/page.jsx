8b950b6a5c01fee040c415e77931a4cbcfe2385b
bcorsi <briana.corso@colliers.com>
2026-04-15 15:05:20 -0700

    Update page.jsx

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,16 +1,8 @@
 'use client';
-
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { createClient } from '@/lib/supabase';
-import Link from 'next/link';
+import { useParams } from 'next/navigation';
 import PropertyDetail from '@/components/PropertyDetail';
 
-export default function PropertyDetailPage({ params }) {
-  const { id } = params;
-  return (
-    <div>
-      <PropertyDetail id={id} inline={false} />
-    </div>
-  );
+export default function PropertyPage() {
+  const { id } = useParams();
+  return <PropertyDetail id={id} />;
 }
